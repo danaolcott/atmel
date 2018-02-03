bf781ce2f4d1fcfb6af45c6ab9e1dde74cb25d42
danaolcott <danaolcott@gmail.com>
2018-02-02 23:29:49 -0800

    continue updates to tempSensor project for attiny85.  it works but not that consistent.

--- source/attiny85/tempSensor/main.c
+++ source/attiny85/tempSensor/main.c
@@ -47,6 +47,9 @@ listed in nextState
 
 State_t fsm[num states]
 
+150 = 73deg
+155 = 78deg
+
 
 
 
@@ -96,6 +99,12 @@ State_t fsm[num states]
 #define TCNT0_R          (*((volatile unsigned char*)(0x32 + IO_OFFSET)))
 #define TIFR_R           (*((volatile unsigned char*)(0x38 + IO_OFFSET)))
 
+
+//ADC
+#define ADCMUX_R         (*((volatile unsigned char*)(0x07 + IO_OFFSET)))
+#define ADCSRA_R         (*((volatile unsigned char*)(0x06 + IO_OFFSET)))
+#define ADCH_R           (*((volatile unsigned char*)(0x05 + IO_OFFSET)))       //bits 01 - top 2 in 10bit
+#define ADCL_R           (*((volatile unsigned char*)(0x04 + IO_OFFSET)))       //bits 0-7
 
 
 //////////////////////////////////////////
@@ -150,9 +159,11 @@ State_t currentState;
 
 ///////////////////////////////
 //ADC Variables, flags... etc
-//flag to check if the basetemp needs
-//to get set for temp sensor adc reading
-unsigned char gBaseTempFlag = 0;    
+//gReferenceADC reading = base reading
+//for all comparisons
+static unsigned int gReferenceADC;
+static unsigned int gReferenceMV;
+static unsigned int gReferenceTEMP;
 
 
 
@@ -167,6 +178,8 @@ void Timer0_init(void);
 void Timer0_OCCA_init(void);
 
 void ADC1_init(void);       //ADC1 on PB2 - Pin 7
+unsigned int ADC1_read(void);
+
 
 //button debounce
 void Waste_CPU(unsigned int temp);
@@ -191,6 +204,18 @@ ISR(TIMER0_COMPA_vect)
 }
 
 
+/////////////////////////////////////////
+//ADC Interrupt
+//see the following .h file
+//for isr definitions:
+//  /usr/lib/avr/include/avr/iotnx4.h
+// 
+//ISR(ADC_vect)
+//{
+//    ADCSRA_R |= BIT4;       //clear interrupt
+//}
+
+
 ///////////////////////////////////////////
 //ISR - Buttons on PCINT0 and PCINT1
 //shared interrupt??
@@ -208,8 +233,6 @@ ISR(PCINT0_vect)
     //PB0 - down
     if (!val0)
     {
-        PORTB_DATA_R ^= BIT3;
-
         flashState = 0;
     }
 
@@ -235,12 +258,14 @@ int main()
 {
     //init pins as output
     GPIO_init();
-
-    //init one or the other
     Timer0_OCCA_init();
-    //Timer0_init();
-
     Button_init();
+    ADC1_init(); 
+
+    //init adc variables
+    gReferenceADC = 0;
+    gReferenceMV = 0x00;
+    gReferenceTEMP = 0x00;
 
     while(1)
     {
@@ -397,6 +422,152 @@ void Timer0_OCCA_init(void)
 
 
 
+
+
+///////////////////////////////////////////////
+//ADC1 on PB2 on Pin7
+//From what I can tell, the main registers 
+//of interest are:
+//PORTB reg - Bit 2 - config as input
+
+//starting page 122 of the datasheet
+//registers defined on page 134, Section 17.13
+//
+//
+//general process:
+//set the ref voltage in ADCMUX - REFS 2:0
+//enable the ADC by setting the ADC enable bit in ADCSRA (ADEN)
+//data is available in registers ADCH and ADCL
+//data is right justified
+//can left justify using the ADLAR bit in ADMUX
+//
+//ADCSRA
+//ADCSRB - not sure we need this one.
+//DIDR0 - digital input disable register
+
+//ADMUX
+//ADCH and ADCL - resultss go here
+//
+//1 - ADMUX - 0x07 - write 0000 0001 = 0x01
+//bit 7-6 - REFS - set to 000 - use Vcc as ref
+//bit 5 - ADLAR - justify - set as 0 - rt justify
+//bits 0 - 3 - MUX - ADC1 - single ended input - write 0001
+//
+//2 - ADCSRA - Control and status register A - 0x06
+//bit 7 - ADEN - enable adc, write to 1 to enable adc
+//bit 6 - ADSC - start the conversion - write 1 to start conversion. 
+//              poll it till it goes low.  when low, the conversion is done
+//
+//bit 5 - ADATE - ADC auto trigger enable.  write this bit one to auto trigger (not sure...)
+//bit 4 - ADIF - interrupt flag, set when conversion is complete.  
+//bit 3 - ADIE - interrupt enable - set to 0
+//bits 0-2 - prescaler - set to 111, prescale = 128.  
+//
+// result = 1010 0111       initial value = 0xA7
+//start the conversion -  1 << 6
+//poll bit 6 until it goes low.
+//
+
+
+//#define ADCMUX_R         (*((volatile unsigned char*)(0x07 + IO_OFFSET)))
+//#define ADCSRA_R         (*((volatile unsigned char*)(0x06 + IO_OFFSET)))
+//#define ADCH_R           (*((volatile unsigned char*)(0x05 + IO_OFFSET)))       //bits 01 - top 2 in 10bit
+//#define ADCL_R           (*((volatile unsigned char*)(0x04 + IO_OFFSET)))       //bits 0-7
+
+//if using auto trigger, the source is selected
+//by setting the trigger bits in ADCSRB
+
+
+void ADC1_init(void)
+{
+   
+    //configure PB2 as input 
+    //PORTB_DIR_R &=~ BIT2;
+
+    //set up the MUX register,        
+    //1 - ADCMUX - 0x07 - write 0000 0001 = 0x01
+    //bit 7-6 - REFS - set to 000 - use Vcc as ref
+    //bit 5 - ADLAR - justify - set as  0 - rt justify
+    //bits 0 - 3 - MUX - ADC1 - single ended input - write 0001
+    //
+
+//    ADMUX = 0x01;      //Vcc as ref, rt justify, ADC1 as single ended input
+
+    //left justify, vcc as ref
+  //  ADMUX = 0x21;      //Vcc as ref, rt justify, ADC1 as single ended input
+
+
+    //left justify, 1.1v ref (bit 7 high, bit 6 low)
+    //1010 0001
+    ADMUX = 0xA1;      //Vcc as ref, rt justify, ADC1 as single ended input
+
+
+//    ADCMUX_R = 0x01;      //Vcc as ref, rt justify, ADC1 as single ended input
+
+//    ADCSRA = ((1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0));  //prescale 128
+//    ADCSRA = ((1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0));  //prescale 64
+
+//prescale 8 works with left justify and using the adc h reg
+
+    ADCSRA = ((0 << ADPS2) | (1 << ADPS1) | (1 << ADPS0));  //prescale 8
+
+//    ADCSRA = ((1 << ADPS2) | (0 << ADPS1) | (0 << ADPS0));  //prescale 16
+
+
+    ADCSRA |= (1 << ADEN);
+
+
+
+}
+
+
+/////////////////////////////////
+//ADC1 read
+//set bit 6 high in the ADCSRA_R
+//to start the conversion,
+//poll the interrupt bit
+//read and return the result
+unsigned int ADC1_read(void)
+{
+    unsigned int result = 0x00;
+    unsigned int temp = 0x00;
+
+    //enable the adc
+//    ADCSRA |= BIT7;
+
+    //write bit 6 high to start the conversion
+    ADCSRA |= (1 << ADSC);
+
+    //wait - poll BIT6 - clear when conversion is complete
+    while (ADCSRA & (1 << ADSC));
+
+    //read the result in ADCH and ADCL
+//    result = ADCL & 0x03;
+//    result = result << 8;
+
+//    result = (ADCH_R & 0xFF);
+//    result = result << 2;
+
+    result = (ADCL_R & 0x03);
+    temp = (ADCH_R & 0xFF) << 2;
+    result |= temp;
+
+
+    
+    //this works with left justify
+//    result = ADCH & 0xFF;
+
+
+
+
+    return result;
+
+}
+
+
+
+
+
 /////////////////////////////////////
 void Waste_CPU(unsigned int temp)
 {
@@ -418,28 +589,169 @@ void Dummy_Function(void)
 
 
 
+/////////////////////////////////////
+//use function 0 to set the 
+//base temp, flash two leds
+//alternating.  read the ADC
+//and set the base value
 void function0(void)
 {
-    PORTB_DATA_R ^= BIT4;
-    Delay(100);
+    gReferenceADC = ADC1_read() & 0x3FF;
+    gReferenceMV = gReferenceADC * 1100 / 1024;
+    gReferenceTEMP = (gReferenceMV - 500) / 10;     //centigrade
+    gReferenceTEMP = ((gReferenceTEMP * 9 / 5) + 32) & 0x7F;
+
+    PORTB_DATA_R |= BIT3;
+    PORTB_DATA_R &=~ BIT4;
+    Delay(200);
+    PORTB_DATA_R |= BIT4;
+    PORTB_DATA_R &=~ BIT3;
+    Delay(200);
+
+
 }
 
+
+
+///////////////////////////////////
+//function 1 - 
+//use this as a comparison with current
+//reading and base reading....
+//if current is less than base, flash yellow
+//if current is higher than base, flash red
 void function1(void)
 {
-    PORTB_DATA_R ^= BIT4;
-    Delay(200);
+    unsigned int diff = 0;
+    unsigned char i = 0;
+    unsigned int currentADC = ADC1_read() & 0x3FF;
+    unsigned int currentMV = currentADC * 1100 / 1024;
+    unsigned int currentTEMP = (currentMV - 500) / 10;     //centigrade
+    currentTEMP = ((currentTEMP * 9 / 5) + 32) & 0x7F;
+
+
+    if (currentTEMP < gReferenceTEMP)
+    {
+        //flash yellow based on diff
+        diff = gReferenceTEMP - currentTEMP;
+
+        for (i = 0 ; i < diff ; i++)
+        {
+            PORTB_DATA_R &=~ BIT4;
+            PORTB_DATA_R |= BIT3;
+            Delay(50);
+            PORTB_DATA_R &=~ BIT4;
+            PORTB_DATA_R &=~ BIT3;
+            Delay(400);
+        }
+
+        Delay(600);
+    }
+
+
+    else
+    {
+        //flash red based on diff
+        diff = currentTEMP - gReferenceTEMP;
+
+        for (i = 0 ; i < diff ; i++)
+        {
+            PORTB_DATA_R &=~ BIT3;
+            PORTB_DATA_R |= BIT4;
+            Delay(50);
+            PORTB_DATA_R &=~ BIT3;
+            PORTB_DATA_R &=~ BIT4;
+            Delay(400);
+        }
+
+        Delay(600);
+    }
 }
 
+
+/////////////////////////////////////
+//flash yellow or red based on diff with
+//base temp 65 deg.
+//
 void function2(void)
 {
-    PORTB_DATA_R ^= BIT4;
-    Delay(500);
+    unsigned int diff = 0;
+    unsigned char i = 0;
+    unsigned int currentADC = ADC1_read() & 0x3FF;
+    unsigned int currentMV = currentADC * 1100 / 1024;
+    unsigned int currentTEMP = (currentMV - 500) / 10;     //centigrade
+    currentTEMP = ((currentTEMP * 9 / 5) + 32) & 0x7F;      //128 F max
+
+
+    if (currentTEMP < 80)
+    {
+        //flash yellow based on diff
+        diff = 80 - currentTEMP;
+
+        for (i = 0 ; i < diff ; i++)
+        {
+            PORTB_DATA_R &=~ BIT4;
+            PORTB_DATA_R |= BIT3;
+            Delay(50);
+            PORTB_DATA_R &=~ BIT4;
+            PORTB_DATA_R &=~ BIT3;
+            Delay(400);
+        }
+
+        Delay(600);
+    }
+
+
+    else
+    {
+        //flash red based on diff
+        diff = currentTEMP - 80;
+
+        for (i = 0 ; i < diff ; i++)
+        {
+            PORTB_DATA_R &=~ BIT3;
+            PORTB_DATA_R |= BIT4;
+            Delay(50);
+            PORTB_DATA_R &=~ BIT3;
+            PORTB_DATA_R &=~ BIT4;
+            Delay(400);
+        }
+
+        Delay(600);
+    }
 }
 
+///////////////////////////////
+//read the value on the adc
+//turn led off if low, led on
+//if high
 void function3(void)
 {
-    PORTB_DATA_R ^= BIT4;
+    unsigned int result = 0x00;
+    result = ADC1_read();
+    result = result & 0x3FF;        //10bit only
+    unsigned int i, val = 0;
+
+    //yellow
+    if (result < 512)
+        PORTB_DATA_R &=~ BIT3;
+    else
+        PORTB_DATA_R |= BIT3;
+
+    //flash the red led based on 100x the 
+    //adc reading
+    val = result / 100;
+    for (i = 0 ; i < val ; i++)
+    {
+        PORTB_DATA_R |= BIT4;
+        Delay(50);
+        PORTB_DATA_R &=~ BIT4;
+        Delay(200);        
+    }
+        
+    //flash freq set by the ADC reading.
     Delay(1000);
+
+
 }
 
 
